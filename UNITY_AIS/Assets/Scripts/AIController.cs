aeafeba209c4c16d0edc3c8fb5ff116901bebeb2
sepponevalainen <S.Nevalainen@gmail.com>
2019-01-17 20:16:32 +0200

    * Imported Smooth follow script from standard assets and used it to make camera follow player. * Added AI_Antagonist_1 character. * Created movement script for player. * Created simple temporary chase algorithm for AI character. * Created animation controllers for player and AI character, and made them endless runners.

---
+++ UNITY_AIS/Assets/Scripts/AIController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class AIController : MonoBehaviour
+{
+
+    public Transform Player;
+    int MoveSpeed = 4;
+    int MaxDist = 10;
+    int MinDist = 2;
+
+    private CharacterController controller;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+    }
+
+    void Update()
+    {
+        
+
+        if (Vector3.Distance(transform.position, Player.position) >= MinDist)
+        {
+            transform.LookAt(Player);
+
+            controller.SimpleMove(transform.forward * MoveSpeed);
+
+
+        }
+        else
+        {
+            controller.SimpleMove(Vector3.zero);
+        }
+    }
+}
---
+++ UNITY_AIS/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed = 6.0f;
+    public float rotateSpeed = 8.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+
+        // let the gameObject fall down
+        gameObject.transform.position = new Vector3(0, 5, 0);
+    }
+
+    void FixedUpdate()
+    {
+        // Rotate around y - axis
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+
+        // Move forward / backward
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        controller.SimpleMove(forward * curSpeed);
+  
+    }
+}
